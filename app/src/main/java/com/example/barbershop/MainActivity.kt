7c3cd69a488fa12f929dbc99ab45f5f5cf45822b
LELIK <b_banni@inbox.ru>
2025-09-14 16:51:24 +0300

    The logic of the first screen is set up

--- app/src/main/java/com/example/barbershop/MainActivity.kt
+++ app/src/main/java/com/example/barbershop/MainActivity.kt
@@ -2,16 +2,45 @@ package com.example.barbershop
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var ediTextName: EditText
+    private lateinit var ediTextPassword: EditText
+    private lateinit var  buttonSignIn: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val intent = Intent(this, OrderDetailActivity::class.java)
+
+        initViews()
+
+        buttonSignIn.setOnClickListener {
+            val userName = ediTextName.text.toString().trim()
+            val userPassword = ediTextPassword.text.toString().trim()
+            if (userName.isNotEmpty() && userPassword.isNotEmpty()) {
+                loadNextScreen(userName)
+            } else {
+                Toast.makeText(this, R.string.error_fields_empty, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun loadNextScreen(userName: String) {
+        val intent = MakeOrderActivity().newIntent(this, userName)
         startActivity(intent)
+    }
+
+    private fun initViews() {
+        ediTextName = findViewById(R.id.ediTextName)
+        ediTextPassword = findViewById(R.id.ediTextPassword)
+        buttonSignIn = findViewById(R.id.buttonSignIn)
     }
 }
--- app/src/main/java/com/example/barbershop/MakeOrderActivity.kt
+++ app/src/main/java/com/example/barbershop/MakeOrderActivity.kt
@@ -1,5 +1,7 @@
 package com.example.barbershop
 
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -9,12 +11,12 @@ import androidx.core.view.WindowInsetsCompat
 class MakeOrderActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_make_order)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
+    }
+
+    fun newIntent(context: Context, userName: String): Intent {
+        val intent = Intent(context, MakeOrderActivity::class.java)
+        intent.putExtra("userName", userName)
+        return intent
     }
 }
